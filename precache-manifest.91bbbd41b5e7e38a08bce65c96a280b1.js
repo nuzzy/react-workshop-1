de53767708ad27ab04674f9bb2bbe4a7f49dd08a
Dima Svetlichniy <dsv1kharkiv@gmail.com>
2020-04-14 12:08:25 +0300

    Updates

---
+++ precache-manifest.91bbbd41b5e7e38a08bce65c96a280b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e7e0d80987648beb28fdec8e67d0cad",
+    "url": "/react-workshop-1/index.html"
+  },
+  {
+    "revision": "d7046f61124ad08ba15e",
+    "url": "/react-workshop-1/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "5ba0648e7a540ff8764b",
+    "url": "/react-workshop-1/static/css/main.46f619ec.chunk.css"
+  },
+  {
+    "revision": "d7046f61124ad08ba15e",
+    "url": "/react-workshop-1/static/js/2.bdd99ebb.chunk.js"
+  },
+  {
+    "revision": "5ba0648e7a540ff8764b",
+    "url": "/react-workshop-1/static/js/main.8b4864d8.chunk.js"
+  },
+  {
+    "revision": "19b6b58181136337f1ba",
+    "url": "/react-workshop-1/static/js/runtime~main.d72951a7.js"
+  }
+]);
